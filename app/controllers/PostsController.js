00f0bd471f08c3ccc0bd90499e28935d279c4372
dmostafiz <dev.mostafiz@gmail.com>
2022-10-27 02:32:29 +0600

    post drafting are done

--- app/controllers/PostsController.js
+++ app/controllers/PostsController.js
@@ -17,7 +17,7 @@ module.exports = {
     createPost: async (req, res) => {
         try {
 
-            if(!req?.user?.id) return res.json({ok: false, msg: 'আপনি নিবন্ধত না!'})
+            if (!req?.user?.id) return res.json({ ok: false, msg: 'আপনি নিবন্ধত না!' })
 
             const post = await req.prisma.post.create({
                 data: {
@@ -27,18 +27,61 @@ module.exports = {
 
             consoleLog('Post Created', post)
 
-            return res.json({ok: true, post})
+            return res.json({ ok: true, post })
 
         } catch (error) {
             consoleLog('post create error', error.message)
         }
     },
 
+    getEditingPost: async (req, res) => {
+        const postId = req.params.postId
+
+        try {
+
+            const post = await req.prisma.post.findFirst({
+                where: {
+                    id: postId
+                }
+            })
+
+            consoleLog('editing post', post)
+
+            return res.json({ok: true, post})
+
+        } catch (error) {
+            consoleLog('get editing post error', error.message)
+        }
+    },
+
     updatePost: async (req, res) => {
+
+        try {
+
+            if (!req.user?.id) return res.json({ ok: false, msg: 'আপনি অথেনটিক নন!' })
+
+            const post = await req.prisma.post.update({
+                where: {
+                    id: req?.body?.id
+                },
+                data: {
+                    title: req?.body?.title,
+                    content: req?.body?.content,
+                    status: req?.body?.status
+                }
+            })
+
+            consoleLog('Post Updated', post)
+
+            return res.json({ ok: true, post })
+
+        } catch (error) {
+            consoleLog('post update error', error.message)
+        }
 
     },
 
     deletePost: async (req, res) => {
-        
+
     },
 }
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express")
-const { createPost } = require("../app/controllers/PostsController")
+const { createPost, updatePost, getEditingPost } = require("../app/controllers/PostsController")
 const authMiddleware = require("../app/middlewares/authMiddleware")
 const router = Router()
 
 router.post('/', [authMiddleware], createPost)
+router.get('/editing_post/:postId', [authMiddleware], getEditingPost)
+router.post('/update', [authMiddleware], updatePost)
 
 module.exports = router
