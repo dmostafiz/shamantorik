6067758da773779f9ec7f9f8b936cfd2a9c067af
dmostafiz <dev.mostafiz@gmail.com>
2022-11-13 13:05:01 +0600

    sd

--- app/controllers/CategoryController.js
+++ app/controllers/CategoryController.js
@@ -37,7 +37,7 @@ module.exports = {
     getTopCategories: async (req, res) => {
 
         const categories = await req.prisma.category.findMany({
-            take: 10,
+            take: 12,
             orderBy: {
                 rank: 'desc'
             },
@@ -103,23 +103,8 @@ module.exports = {
 
             const categories = await req.prisma.category.createMany({
                 data: [
-                    { name: 'কবিতা', slug: 'কবিতা', isAdmin: false },
-                    { name: 'গল্প', slug: 'গল্প', isAdmin: false },
-                    { name: 'উপন্যাস', slug: 'উপন্যাস', isAdmin: false },
-                    { name: 'সাহিত্য', slug: 'সাহিত্য', isAdmin: false },
-                    { name: 'সমসাময়িক', slug: 'সমসাময়িক', isAdmin: false },
-                    { name: 'মুক্তিযুদ্ধ', slug: 'মুক্তিযুদ্ধ', isAdmin: false },
-                    { name: 'লেখাপড়া', slug: 'লেখাপড়া', isAdmin: false },
-                    { name: 'বিজ্ঞান-প্রযুক্তি', slug: 'বিজ্ঞান-প্রযুক্তি', isAdmin: false },
-                    { name: 'ইতিহাস', slug: 'ইতিহাস', isAdmin: false },
-                    { name: 'চিন্তাধারা', slug: 'চিন্তাধারা', isAdmin: false },
-                    { name: 'সংস্কৃতি', slug: 'সংস্কৃতি', isAdmin: false },
-                    { name: 'ছবি ব্লগ', slug: 'ছবি-ব্লগ', isAdmin: false },
-                    { name: 'রাজনীতি', slug: 'রাজনীতি', isAdmin: false },
-                    { name: 'ভ্রমণ কাহিনী', slug: 'ভ্রমণ-কাহিনী', isAdmin: false },
-                    { name: 'দেশ-বিদেশ', slug: 'দেশ-বিদেশ', isAdmin: false },
-                    { name: 'বই রিভিউ', slug: 'বই-রিভিউ', isAdmin: false },
-                    { name: 'মুভি রিভিউ', slug: 'মুভি-রিভিউ', isAdmin: false },
+                    { name: 'জীবন', slug: 'জীবন', isAdmin: false },
+
                 ]
             })
 
